e77c0d0d4a07748f53556dea547ad64b8c7e9c15
Alex Sandri <me@alexsandri.com>
2020-03-18 10:31:13 +0100

    Fix: Update

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const cacheName = "static-v1171";
+const cacheName = "static-v1172";
 self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
     "/",
     "/account",
--- src/ts/service_workers/sw.ts
+++ src/ts/service_workers/sw.ts
@@ -1,4 +1,4 @@
-const cacheName : string = "static-v1171";
+const cacheName : string = "static-v1172";
 
 self.addEventListener("install", (e : any) =>
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
