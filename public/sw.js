9af8025d771326ffded0438a1b9758b71e14adf4
Alex Sandri <me@alexsandri.com>
2020-03-22 11:18:17 +0100

    Fix wrong keyboard shortcuts hints

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const cacheName = "static-v1230";
+const cacheName = "static-v1231";
 self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
     "/",
     "/account",
