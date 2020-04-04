6dbb80bbdea19d699d09197d0099f2f514242f62
Alex Sandri <me@alexsandri.com>
2020-04-04 17:33:01 +0200

    Improve UI and remove useless CSS

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const cacheName = "static-v1447";
+const cacheName = "static-v1450";
 self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
     "/",
     "/account",
@@ -8,7 +8,7 @@ self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(
     "/privacy",
     "/cookies",
     "/accessibility",
-    "/assets/css/bundle.08c5941afe749cef1acd88c415d1cc7f.css",
+    "/assets/css/bundle.7965dcb9e2adcc0a0acdf9172de7dff8.css",
     "/assets/js/home.4130ae52ea90f5308973.js",
     "/assets/js/account.8185404be80853be5ac3.js",
     "/assets/js/settings.db3b6746ef2666a9fefc.js",
