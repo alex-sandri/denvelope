01d8c9827e4d09d0064d641389bc4f1cfa352292
Alex Sandri <me@alexsandri.com>
2020-04-05 14:00:42 +0200

    Update

--- src/ts/service_workers/sw.ts
+++ src/ts/service_workers/sw.ts
@@ -1,4 +1,4 @@
-const cacheName : string = "static-v1462";
+const cacheName : string = "static-v1466";
 
 self.addEventListener("install", (e : any) =>
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
@@ -9,7 +9,7 @@ self.addEventListener("install", (e : any) =>
         "/privacy",
         "/cookies",
         "/accessibility",
-        "/assets/css/bundle.976c2d3f2bf3af2d2232bf6978b672a6.css",
+        "/assets/css/bundle.8d4167ebfbf830375896e878f448afbe.css",
         "/assets/js/home.4130ae52ea90f5308973.js",
         "/assets/js/account.8185404be80853be5ac3.js",
         "/assets/js/settings.db3b6746ef2666a9fefc.js",
