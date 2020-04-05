26eccfb2d7ede4969667f471a977a85a6ec58db1
Alex Sandri <me@alexsandri.com>
2020-04-05 15:46:43 +0200

    Fix: Image not loading while offline on the home page

--- src/ts/service_workers/sw.ts
+++ src/ts/service_workers/sw.ts
@@ -1,4 +1,4 @@
-const cacheName : string = "static-v1469";
+const cacheName : string = "static-v1470";
 
 self.addEventListener("install", (e : any) =>
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
