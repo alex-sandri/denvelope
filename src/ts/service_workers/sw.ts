52551234ad7d672457bcdd778c74d0bedcb57c34
Alex Sandri <me@alexsandri.com>
2020-04-14 13:42:35 +0200

    Update GitHub link in the home page

--- src/ts/service_workers/sw.ts
+++ src/ts/service_workers/sw.ts
@@ -1,4 +1,4 @@
-const cacheName : string = "static-v1668";
+const cacheName : string = "static-v1671";
 
 self.addEventListener("install", (e : any) =>
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
@@ -9,7 +9,7 @@ self.addEventListener("install", (e : any) =>
         "/privacy",
         "/cookies",
         "/accessibility",
-        "/assets/css/bundle.1272933b99c4f44f1e38ed17a482775d.css",
+        "/assets/css/bundle.31b07b7c660699ccdd3fb0c6e65a2528.css",
         "/assets/js/home.c2add5cbc47cd567f000.js",
         "/assets/js/account.a7511888692866b4d78e.js",
         "/assets/js/settings.52f642e53e3abf681e9b.js",
