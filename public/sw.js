9ceef2fb322587d8162d382f89248495dcbaa89a
Alex Sandri <me@alexsandri.com>
2020-04-24 15:42:14 +0200

    Block possibility to directly delete files unless they are compressed folders

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const cacheName = "static-v1945";
+const cacheName = "static-v1946";
 self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
     "/",
     "/account",
--- src/ts/service_workers/sw.ts
+++ src/ts/service_workers/sw.ts
@@ -1,4 +1,4 @@
-const cacheName : string = "static-v1945";
+const cacheName : string = "static-v1946";
 
 self.addEventListener("install", (e : any) =>
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
