e41d7b5c15e6f1a4b36788b3bbc4214965735581
Alex Sandri <me@alexsandri.com>
2020-04-27 13:27:38 +0200

    Fix wrong link to see the largest files

--- src/ts/service_workers/sw.ts
+++ src/ts/service_workers/sw.ts
@@ -1,4 +1,4 @@
-const cacheName : string = "static-v1979";
+const cacheName : string = "static-v1980";
 
 self.addEventListener("install", (e : any) =>
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
