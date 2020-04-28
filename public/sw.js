9da5ab2280422bb1bd4fdcc8476e1692e56baa8e
Alex Sandri <me@alexsandri.com>
2020-04-28 18:16:11 +0200

    Add privacy section to settings menu

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const cacheName = "static-v2009";
+const cacheName = "static-v2010";
 self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
     "/",
     "/account",
