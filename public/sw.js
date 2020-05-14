a8bb46d9b6ce63f1ca521616910bb174a243c9fa
Alex Sandri <me@alexsandri.com>
2020-05-14 14:23:48 +0200

    Add placeholder values for each plan's storage space

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const cacheName = "static-v2224";
+const cacheName = "static-v2225";
 self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
     "/",
     "/account",
