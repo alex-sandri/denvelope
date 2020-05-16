255195ff058bad9784d386f309e9099864187c08
Alex Sandri <me@alexsandri.com>
2020-05-16 12:31:19 +0200

    Remove useless comment

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -503,7 +503,6 @@ app.post("/stripeWebhooks", async (request, response) =>
     try { event = stripe.webhooks.constructEvent(request.body, signature, STRIPE_WEBHOOK_SECRET); }
     catch (err) { response.status(400).send(`Webhook Error: ${err.message}`); }
 
-    // Handle the event
     switch ((<Stripe.Event>event).type)
     {
         case "customer.subscription.deleted":
