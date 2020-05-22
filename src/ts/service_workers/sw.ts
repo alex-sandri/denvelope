99ab1c8e2d8a89db148112218cbbcf32ae77f42f
Alex Sandri <me@alexsandri.com>
2020-05-22 16:38:04 +0200

    Fix: Settings do not load if language is in lowercase

--- src/ts/service_workers/sw.ts
+++ src/ts/service_workers/sw.ts
@@ -1,4 +1,4 @@
-const cacheName : string = "static-v2382";
+const cacheName : string = "static-v2383";
 
 self.addEventListener("install", (e : any) =>
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
