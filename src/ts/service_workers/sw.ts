cf1bd5abdc02c7861e749e49b3fd03ae6bdc00ba
Alex Sandri <me@alexsandri.com>
2020-05-30 15:00:36 +0200

    Update manifest version

--- src/ts/service_workers/sw.ts
+++ src/ts/service_workers/sw.ts
@@ -1,4 +1,4 @@
-const cacheName : string = "static-v2438";
+const cacheName : string = "static-v2439";
 
 self.addEventListener("install", (e : any) =>
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
