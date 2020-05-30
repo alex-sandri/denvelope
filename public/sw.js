2df3b748f07169c3ee896d20ead7a403e8689c12
Alex Sandri <me@alexsandri.com>
2020-05-30 15:17:09 +0200

    Rename from add ... to create ... to better represent the feature

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const cacheName = "static-v2439";
+const cacheName = "static-v2440";
 self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
     "/",
     "/account",
