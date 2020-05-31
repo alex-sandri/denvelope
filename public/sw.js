1c3db3df72933bf56ee80e78a5818d27aba80fcf
Alex Sandri <me@alexsandri.com>
2020-05-31 14:08:20 +0200

    Add contact us button in the info section of the settings page

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const cacheName = "static-v2441";
+const cacheName = "static-v2442";
 self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
     "/",
     "/account",
