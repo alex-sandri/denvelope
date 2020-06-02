6fea5cc2f05b18dde4a4eade8ae43d2fcba6843d
Alex Sandri <me@alexsandri.com>
2020-06-02 19:08:03 +0200

    Remove unused code

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const cacheName = "static-v2521";
+const cacheName = "static-v2522";
 self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
     "/",
     "/account",
--- src/ts/service_workers/sw.ts
+++ src/ts/service_workers/sw.ts
@@ -1,4 +1,4 @@
-const cacheName : string = "static-v2521";
+const cacheName : string = "static-v2522";
 
 self.addEventListener("install", (e : any) =>
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
