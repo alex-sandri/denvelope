3cd02b7d602618beb74c28ad0fde51c6e76d36fa
Alex Sandri <me@alexsandri.com>
2020-06-04 18:36:31 +0200

    Set context menu max width

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const cacheName = "static-v2577";
+const cacheName = "static-v2578";
 self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
     "/",
     "/account",
--- src/ts/service_workers/sw.ts
+++ src/ts/service_workers/sw.ts
@@ -1,4 +1,4 @@
-const cacheName : string = "static-v2577";
+const cacheName : string = "static-v2578";
 
 self.addEventListener("install", (e : any) =>
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
