2b11b6dcd262f8f61937b39a8ce61b1f534f88eb
Alex Sandri <me@alexsandri.com>
2020-06-04 13:01:41 +0200

    Update

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const cacheName = "static-v2547";
+const cacheName = "static-v2548";
 self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
     "/",
     "/account",
@@ -11,7 +11,7 @@ self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(
     "/accessibility",
     "/assets/css/bundle.256022146d95e36ac7dead7518dd2892.css",
     "/assets/js/home.310527656791eb726007.js",
-    "/assets/js/account.fb78d741fa1a14994050.js",
+    "/assets/js/account.806dcc894e03cac55c96.js",
     "/assets/js/settings.f114ccaf2d20d7075708.js",
     "/__/firebase/7.14.6/firebase-app.js",
     "/__/firebase/7.14.6/firebase-auth.js",
--- src/ts/service_workers/sw.ts
+++ src/ts/service_workers/sw.ts
@@ -1,4 +1,4 @@
-const cacheName : string = "static-v2547";
+const cacheName : string = "static-v2548";
 
 self.addEventListener("install", (e : any) =>
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
@@ -12,7 +12,7 @@ self.addEventListener("install", (e : any) =>
         "/accessibility",
         "/assets/css/bundle.256022146d95e36ac7dead7518dd2892.css",
         "/assets/js/home.310527656791eb726007.js",
-        "/assets/js/account.fb78d741fa1a14994050.js",
+        "/assets/js/account.806dcc894e03cac55c96.js",
         "/assets/js/settings.f114ccaf2d20d7075708.js",
         "/__/firebase/7.14.6/firebase-app.js",
         "/__/firebase/7.14.6/firebase-auth.js",
