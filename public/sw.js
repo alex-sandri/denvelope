22a0a6fbdc9553559a47273d0101f25351dddd10
Alex Sandri <me@alexsandri.com>
2020-06-06 20:04:52 +0200

    Underline the links in the footer on focus

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const cacheName = "static-v2654";
+const cacheName = "static-v2655";
 self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
     "/",
     "/account",
@@ -9,7 +9,7 @@ self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(
     "/privacy",
     "/cookies",
     "/accessibility",
-    "/assets/css/bundle.ca87fc944b641128cc5243bbe2ac8dba.css",
+    "/assets/css/bundle.aca0f49135233732bccf89b77fa35e4e.css",
     "/assets/js/home.7fa9104aaf2d8ec4b787.js",
     "/assets/js/account.d06447385e7c863b0d48.js",
     "/assets/js/settings.7b0a35572302276f1eba.js",
--- src/ts/service_workers/sw.ts
+++ src/ts/service_workers/sw.ts
@@ -1,4 +1,4 @@
-const cacheName : string = "static-v2654";
+const cacheName : string = "static-v2655";
 
 self.addEventListener("install", (e : any) =>
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
@@ -10,7 +10,7 @@ self.addEventListener("install", (e : any) =>
         "/privacy",
         "/cookies",
         "/accessibility",
-        "/assets/css/bundle.ca87fc944b641128cc5243bbe2ac8dba.css",
+        "/assets/css/bundle.aca0f49135233732bccf89b77fa35e4e.css",
         "/assets/js/home.7fa9104aaf2d8ec4b787.js",
         "/assets/js/account.d06447385e7c863b0d48.js",
         "/assets/js/settings.7b0a35572302276f1eba.js",
