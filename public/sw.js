37a2e5a75125e2d7c34654bc0d30f6d2a2436e1b
Alex Sandri <me@alexsandri.com>
2020-06-07 13:58:18 +0200

    Fix: select colors do not change on hover

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const cacheName = "static-v2666";
+const cacheName = "static-v2667";
 self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
     "/",
     "/account",
@@ -9,7 +9,7 @@ self.addEventListener("install", (e) => e.waitUntil(caches.open(cacheName).then(
     "/privacy",
     "/cookies",
     "/accessibility",
-    "/assets/css/bundle.821b9994094f7f13ed3d3c933efe178d.css",
+    "/assets/css/bundle.adc75caeaf90c497b406f013e239fd6d.css",
     "/assets/js/home.7fa9104aaf2d8ec4b787.js",
     "/assets/js/account.d06447385e7c863b0d48.js",
     "/assets/js/settings.595511e053bcd400cc44.js",
