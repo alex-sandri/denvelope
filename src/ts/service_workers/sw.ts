48e91e490876d041d66dc8ec381b64a4b187d68b
Alex Sandri <me@alexsandri.com>
2020-06-09 19:54:53 +0200

    Fix: can't access the context menu while a file is open

--- src/ts/service_workers/sw.ts
+++ src/ts/service_workers/sw.ts
@@ -1,4 +1,4 @@
-const cacheName : string = "static-v2765";
+const cacheName : string = "static-v2766";
 
 self.addEventListener("install", (e : any) =>
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
