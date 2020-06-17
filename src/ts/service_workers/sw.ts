93f638709e3789ab1d1fd40f6ba4e34de29d3728
Alex Sandri <me@alexsandri.com>
2020-06-17 11:53:38 +0200

    Enable year billing period

--- src/ts/service_workers/sw.ts
+++ src/ts/service_workers/sw.ts
@@ -1,4 +1,4 @@
-const cacheName : string = "static-v2866";
+const cacheName : string = "static-v2867";
 
 self.addEventListener("install", (e : any) =>
 	e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
