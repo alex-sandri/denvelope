8dd4c9a648b586241d548fd2d17364d1e93d0456
Alex Sandri <me@alexsandri.com>
2020-06-24 12:53:19 +0200

    Update sw version

--- src/ts/service_workers/sw.ts
+++ src/ts/service_workers/sw.ts
@@ -1,6 +1,6 @@
 import { static as staticAssets, dynamic } from "../config/assets.json";
 
-const cacheName : string = "static-v3044";
+const cacheName : string = "static-v3045";
 
 self.addEventListener("install", (e : any) =>
 	e.waitUntil(caches.open(cacheName).then(cache => cache.addAll([
