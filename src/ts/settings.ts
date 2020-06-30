6ebe6a1e65e8d3fc3d9d3e2122f441da2b4ac93f
Alex Sandri <me@alexsandri.com>
2020-06-30 14:28:10 +0200

    Update redirectToCheckout implementation

--- src/ts/settings.ts
+++ src/ts/settings.ts
@@ -311,22 +311,13 @@ window.addEventListener("userready", () =>
 		{
 			modal.HideAndRemove();
 
-			stripe.redirectToCheckout(<any>{
-				lineItems: [
-					{
-						price: Config.Pricing.Plan(
-							<Config.PlanName>plans.querySelector(".selected").getAttribute("data-max-storage"),
-							Translation.Currency,
-							<Config.BillingPeriod>document.querySelector(".billing-periods .selected").classList[0],
-						).StripePriceId,
-						quantity: 1,
-					},
-				],
-				mode: "subscription",
-				locale: Translation.ShortLanguage,
-				successUrl: location.href,
-				cancelUrl: location.href,
+			const result = await functions.httpsCallable("createCheckoutSession")({
+				maxStorage: plans.querySelector(".current").getAttribute("data-max-storage"),
+				currency: Translation.Currency,
+				billingPeriod: document.querySelector(".billing-periods .selected").classList[0],
 			});
+
+			stripe.redirectToCheckout({ sessionId: result.data.sessionId });
 		};
 
 		modal.Show(true);
