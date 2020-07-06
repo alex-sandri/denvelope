9aef8ddb51ba98c79605a0ce554b414cfff5634b
Alex Sandri <me@alexsandri.com>
2020-07-06 20:00:22 +0200

    Export just one config so an array is not needed

--- webpack.sw.config.ts
+++ webpack.sw.config.ts
@@ -1,28 +1,26 @@
 import path from "path";
 import * as webpack from 'webpack';
 
-const config: webpack.Configuration[] = [
-    {
-        entry: "./src/ts/sw/sw.ts",
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    loader: "ts-loader",
-                },
-            ],
-        },
-        resolve: {
-            extensions: [
-                ".ts",
-                ".js"
-            ],
-        },
-        output: {
-            filename: "sw.js",
-            path: path.resolve(__dirname, "public"),
-        },
-    }
-];
+const config: webpack.Configuration = {
+    entry: "./src/ts/sw/sw.ts",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader",
+            },
+        ],
+    },
+    resolve: {
+        extensions: [
+            ".ts",
+            ".js"
+        ],
+    },
+    output: {
+        filename: "sw.js",
+        path: path.resolve(__dirname, "public"),
+    },
+};
 
 export default config;
